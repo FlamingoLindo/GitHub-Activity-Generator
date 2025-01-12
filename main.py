73726e5dec36db41d1417ac30e62fc08b9735d3d
FlamingoLindo <101421364+FlamingoLindo@users.noreply.github.com>
2025-01-12 17:44:00 -0300

    2025-01-12 17:44:00

--- main.py
+++ main.py
@@ -5,21 +5,20 @@ from git_push import git_push
 from write_file import write_file
 
 def activity_generator():
-    # Generate random commit amount
-    rand_commit_amount = random.randint(5, 20)
-    print(f"Executing {rand_commit_amount} commits with a delay of 1 to 8 hours between commits.")
+    rand_commit_amount = random.randint(5, 20)  # Between 5 and 20 commits
+    print(f"Executing {rand_commit_amount} commits with a delay of 5 to 27 minutes between commits.")
     
     for i in range(rand_commit_amount):
         write_file()
         git_push()
         rand_time_delay = random.randint(5, 27)  # Delay between 5 and 27 minutes
-        print(f"Commit {i + 1}/{rand_commit_amount} done. Next commit in {rand_time_delay} hour(s).")
+        print(f"Commit {i + 1}/{rand_commit_amount} done. Next commit in {rand_time_delay} minute(s).")
         time.sleep(rand_time_delay * 60)  # Convert minutes to seconds
 
-# Schedule the task at a specific time
-schedule.every().day.at("17:43").do(activity_generator)
+# Schedule the task
+schedule.every().day.at("17:44").do(activity_generator)
 
-# Run the schedule loop
+# Run the scheduler
 print("Scheduler started. Waiting for tasks...")
 while True:
     schedule.run_pending()
